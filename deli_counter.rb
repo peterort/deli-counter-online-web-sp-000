0f47189d9cc8a82b1dc62d091e9b997144068873
Peter Ort <peter@peterort.com>
2020-04-23 16:58:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(person)
   if person == []
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{person.size-1}. #{person}"
+    puts "The line is currently: #{person.size-1}"
   #shows current place in line
 
   end
