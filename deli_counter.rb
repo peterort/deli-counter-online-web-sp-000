f2e568db1ed219868173d7752687d78846015466
Peter Ort <peter@peterort.com>
2020-04-23 16:53:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 
 def line(person)
-
+  if person.find? == true
+    puts "The line is currently #{person.size+1}"
   #shows current place in line
   #if nobody in line,
   puts "The line is currently empty."
